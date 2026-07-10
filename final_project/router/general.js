21a6c45c6111033205b4e3f8ac9a5f4c9be31596
Cəmil İbrahimli <cemil.ibrahimli@etsn.local>
2026-07-10 11:04:10 +0400

    Implement get books by author endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,9 +28,18 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const author = req.params.author;
+
+  const booksByAuthor = Object.values(books).filter((book) => {
+    return book.author === author;
+  });
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  }
+
+  return res.status(404).json({ message: "No books found for this author" });
 });
 
 // Get all books based on title
