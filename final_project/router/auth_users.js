eb574fcee01b82da431273d0e4e2674d5f9b9a72
Cəmil İbrahimli <cemil.ibrahimli@etsn.local>
2026-07-10 14:01:26 +0400

    Implement user login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -12,9 +12,32 @@ const authenticatedUser = (username, password) => {
   return users.some((user) => user.username === username && user.password === password);
 };
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({ message: "Username and password are required" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+
+    return res.status(200).json({ message: "User successfully logged in" });
+  }
+
+  return res.status(208).json({ message: "Invalid Login. Check username and password" });
 });
 
 // Add a book review
